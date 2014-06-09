0245ebdc2164a6ce2ee92b1598607d8864d69827
Osni Carfi <osnicarfi@hotmail.com>
2014-06-09 02:48:28 -0300

    commit before adding votes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Projeto::Application.routes.draw do
+  resources :articles
+
   devise_for :users
   root "pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140607201020) do
+ActiveRecord::Schema.define(version: 20140607215533) do
+
+  create_table "articles", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "category_id"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
