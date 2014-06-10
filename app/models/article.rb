48938f4124f7258629546d4258f5210cca6f604e
Osni Carfi <osnicarfi@hotmail.com>
2014-06-09 23:11:48 -0300

    migrations corrected and paperclip added

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article < ActiveRecord::Base
 	belongs_to :user
 	has_many :reviews, dependent: :destroy
 	validates :category, presence: true
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140609203058) do
+ActiveRecord::Schema.define(version: 20140610014055) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20140609203058) do
     t.datetime "updated_at"
     t.integer  "category_id"
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "articles", ["user_id"], name: "index_articles_on_user_id"
