57924e34b3f9d3a9f862c6f398613c114bee5ef8
Osni Carfi <osnicarfi@hotmail.com>
2014-06-27 02:42:39 -0300

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,6 +41,7 @@ Projeto::Application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
+  
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
